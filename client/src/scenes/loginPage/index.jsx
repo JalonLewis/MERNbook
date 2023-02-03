eb4db707f68c1a06529b22e6bdc7ca786fe7e144
Jalon <jalonlewis123@gmail.com>
2023-02-03 14:50:13 -0600

    basic frontend working as intended

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
-            Welcome to MERNbook, the most unique social media platform in existence which is totally not a basic facebook clone...
+            Welcome to MERNbook, the most unique social media platform in existence which is totally not a facebook clone...
         </Typography>
         <Form />
       </Box>
