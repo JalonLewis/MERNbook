c12bf72c2a5b456f394d1aa8e968b665dcbcac8a
Jalon <jalonlewis123@gmail.com>
2023-02-05 15:50:10 -0600

    user images can now be opened from pfp

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -5,13 +5,13 @@ const URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001";
 const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
-      <img
+      <a href={`https://mernbook-server.onrender.com/assets/${image}`} target="_blank" rel="noreferrer"><img
         style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
         alt="user"
         src={`${URL}/assets/${image}`}
-      />
+      /></a>
     </Box>
   );
 };
